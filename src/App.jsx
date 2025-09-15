d0dc2240d43f116891ee57c6ef3eead7f13f1bcc
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-16 02:05:09 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Counter from './Counter';
 import Batsman from './Batsman';
 import Bowler from './Bowler';
+import Friends from './Friends';
 import Users from './Users';
 import './App.css'
+import { Suspense } from 'react';
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+
+const fetchFriends = async () =>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  return res.json();
+}
 
 function App() {
-
+  const friendsPromise = fetchFriends();
+  
   function handleClick(){
     alert("I am clicked");
   }
@@ -24,7 +35,14 @@ function App() {
     <>
       <h3>Vite + React</h3>
 
-      <Users></Users>
+      {/* <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers = {fetchUsers}></Users>
+      </Suspense> */}
+
+      <Suspense fallback={<h3>Friends are comin'</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
+
       <Batsman></Batsman>
       <Bowler></Bowler>
       <Counter></Counter>
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,7 +1,10 @@
-export default function Users () {
+import { use } from "react"
+export default function Users({ fetchUsers }) {
+    const users = use(fetchUsers);
+    console.log(users);
     return (
         <div>
-            <h3 className="card">Users: </h3>
+            <h3 className="card">Users: {users.length}</h3>
         </div>
     )
 }
